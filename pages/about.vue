e5f960bd259d118f9f6036b028ff908cd5a04043
Youness Fkhach <fkhachyouness@gmail.com>
2018-02-13 10:17:48 +0000

    initial commit

---
+++ pages/about.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>About</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus laboriosam temporibus sit, dicta, quisquam animi voluptatum, eos esse tenetur maxime nobis qui veniam asperiores cumque quidem suscipit id! Blanditiis, debitis.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  beforeMount() {
+    console.log('mounting!..')
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/profile/_id.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Hi num {{ id }}</h1>
+</template>
+
+<script>
+export default {
+  computed: {
+    id() {
+      return this.$route.params.id
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
